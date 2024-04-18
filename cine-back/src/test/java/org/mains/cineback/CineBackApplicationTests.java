e629ed0abec72d443e2dea5fcae6d3ebf6bd20c3
ABDEL KARIM <foikwetkarima@gmail.com>
2024-04-18 08:45:49 +0100

    création du projet backend

---
+++ cine-back/src/test/java/org/mains/cineback/CineBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mains.cineback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
